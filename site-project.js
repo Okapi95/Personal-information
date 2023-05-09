77c61cd252b7f74f5f603f838437c66dffe885be
Okapi <mari.shpak.1995@gmail.com>
2023-05-09 18:29:38 +0300

    доработки, удаление ненужных файлов

--- site-project.js
+++ site-project.js
@@ -1,92 +1,98 @@
-
-const linkAboutMe = document.querySelector('.navigation__link_about');
-const galleryAboutMe = document.querySelector('.gallery');
-linkAboutMe.addEventListener('click', function () {
-   linkHandler(galleryAboutMe);
+const linkAboutMe = document.querySelector(".navigation__link_about");
+const galleryAboutMe = document.querySelector(".gallery");
+linkAboutMe.addEventListener("click", function () {
+  linkHandler(galleryAboutMe);
 });
 
-const linkResume = document.querySelector('.navigation__link_resume');
-const resume = document.querySelector('.resume');
-linkResume.addEventListener('click', function () {
-   linkHandler(resume);
+const linkResume = document.querySelector(".navigation__link_resume");
+const resume = document.querySelector(".resume");
+linkResume.addEventListener("click", function () {
+  linkHandler(resume);
 });
 
-const linkPortfolio = document.querySelector('.navigation__link_portfolio');
-const portfolio = document.querySelector('.portfolio');
-linkPortfolio.addEventListener('click', function () {
-   linkHandler(portfolio);
+const linkPortfolio = document.querySelector(".navigation__link_portfolio");
+const portfolio = document.querySelector(".portfolio");
+linkPortfolio.addEventListener("click", function () {
+  linkHandler(portfolio);
 });
 
-const linkGeneral = document.querySelector('.navigation__link_general');
-const generalInformation = document.querySelector('.section-general');
-linkGeneral.addEventListener('click', function () {
-   linkHandler(generalInformation);
+const linkGeneral = document.querySelector(".navigation__link_general");
+const generalInformation = document.querySelector(".section-general");
+linkGeneral.addEventListener("click", function () {
+  linkHandler(generalInformation);
 });
 
 function linkHandler(selectedSection) {
-   if (selectedSection.classList.contains('visibleJS')) return;
-   selectedSection.classList.remove('hideJS');
-   const visible = document.querySelector('.visibleJS');
-   visible.classList.add('hideJS');
-   visible.classList.remove('visibleJS');
-   selectedSection.classList.add('visibleJS');
+  if (selectedSection.classList.contains("visibleJS")) return;
+  selectedSection.classList.remove("hideJS");
+  const visible = document.querySelector(".visibleJS");
+  visible.classList.add("hideJS");
+  visible.classList.remove("visibleJS");
+  selectedSection.classList.add("visibleJS");
+}
+
+const photosPreviews = document.querySelectorAll(".gallery__photo-preview img");
+const photoPreviewsСomments = document.querySelectorAll(
+  ".gallery__photo-preview span"
+);
+const photoFullСomment = document.querySelector(".gallery.hideJS h2");
+const fullPhoto = document.querySelector(".full-photo");
+let currentIndexFull = 1;
+const switchPhotoByPreview = function (
+  photoPreview,
+  index,
+  photoPreviewsСomment
+) {
+  photoPreview.addEventListener("click", function () {
+    fullPhoto.src = photoPreview.src;
+    photoFullСomment.textContent = photoPreviewsСomment.textContent;
+    currentIndexFull = index;
+  });
+};
+for (let i = 0; i < photosPreviews.length; i++) {
+  switchPhotoByPreview(photosPreviews[i], i, photoPreviewsСomments[i]);
+}
+
+const buttonNext = document.querySelector(".gallery__button-next");
+const buttonPrevious = document.querySelector(".gallery__button-previous");
+const changePhotoByButtonNext = function () {
+  buttonNext.addEventListener("click", function () {
+    currentIndexFull++;
+    if (currentIndexFull >= photosPreviews.length) {
+      currentIndexFull = 0;
+    }
+    fullPhoto.src = photosPreviews[currentIndexFull].src;
+    photoFullСomment.textContent =
+      photoPreviewsСomments[currentIndexFull].textContent;
+  });
+};
+const changePhotoByButtonPrevious = function () {
+  buttonPrevious.addEventListener("click", function () {
+    if (currentIndexFull) {
+      fullPhoto.src = photosPreviews[--currentIndexFull].src;
+      photoFullСomment.textContent =
+        photoPreviewsСomments[currentIndexFull].textContent;
+    }
+  });
 };
 
-let photosPreviews = document.querySelectorAll('.gallery__photo-preview img')
-let photoPreviewsСomments = document.querySelectorAll('.gallery__photo-preview span')
-let photoFullСomment = document.querySelector('.gallery.hideJS h2');
-let fullPhoto = document.querySelector('.full-photo');
-let currentIndexFull = 1; 
-let switchPhotoByPreview = function (photoPreview, index, photoPreviewsСomment) {
-   photoPreview.addEventListener('click', function () {
-      fullPhoto.src = photoPreview.src; 
-      photoFullСomment.textContent = photoPreviewsСomment.textContent;
-      currentIndexFull = index;
-   });
-};
-for (var i = 0; i < photosPreviews.length; i++) {
-   switchPhotoByPreview(photosPreviews[i], i, photoPreviewsСomments[i]);
-};
-
-
-let buttonNext = document.querySelector('.gallery__button-next');
-let buttonPrevious = document.querySelector('.gallery__button-previous');
-let changePhotoByButtonNext = function () {
-   buttonNext.addEventListener ('click', function () {
+const changePhotoKeydown = function () {
+  document.addEventListener("keydown", function (event) {
+    if (event.key === "ArrowLeft") {
+      if (currentIndexFull) {
+        fullPhoto.src = photosPreviews[--currentIndexFull].src;
+      }
+    }
+    if (event.key === "ArrowRight") {
       currentIndexFull++;
       if (currentIndexFull >= photosPreviews.length) {
-         currentIndexFull = 0;
+        currentIndexFull = 0;
       }
       fullPhoto.src = photosPreviews[currentIndexFull].src;
-      photoFullСomment.textContent = photoPreviewsСomments[currentIndexFull].textContent;    
-   })
-};
-
-let changePhotoByButtonPrevious = function () {
-   buttonPrevious.addEventListener('click', function () {
-      if (currentIndexFull) {
-         fullPhoto.src = photosPreviews[--currentIndexFull].src;
-         photoFullСomment.textContent = photoPreviewsСomments[currentIndexFull].textContent;
-      };
-   })
-};
-
-let changePhotoKeydown = function () {
-   document.addEventListener('keydown', function (event) {
-      if (event.key === 'ArrowLeft') {
-         if (currentIndexFull) {
-            fullPhoto.src = photosPreviews[--currentIndexFull].src;
-         };
-      }
-      if (event.key === 'ArrowRight') {
-         currentIndexFull++;
-         if (currentIndexFull >= photosPreviews.length) {
-            currentIndexFull = 0;
-         }
-         fullPhoto.src = photosPreviews[currentIndexFull].src;
-      }
-      photoFullСomment.textContent = photoPreviewsСomments[currentIndexFull].textContent;
-   })
+    }
+    photoFullСomment.textContent =
+      photoPreviewsСomments[currentIndexFull].textContent;
+  });
 };
 
 changePhotoKeydown();
